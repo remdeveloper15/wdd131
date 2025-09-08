3ddee39f96a3e8d6e92973231ebf40207504da11
remdeveloper15 <helamanfranco@hotmail.com>
2025-09-07 20:46:45 -0500

    Week 01

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,7 +1,7 @@
 const year = document.querySelector("#currentyear")
 let lastModification = document.querySelector("#lastModified")
 
-const today = new Date();
+const today = new Date();1
 let last = new Date(document.lastModified);
 
 year.innerHTML = `©${today.getFullYear()} 🪼 Helaman Franco 🪼 Ecuador`;
